68a94a0f36d4481f559ca2923a3ff128455595fc
gaaaa.suuuu <gaaaa.suuuu@gmail.com>
2011-10-14 11:22:56 +0900

    全ファイルアップロード
    Signed-off-by: gaaaa.suuuu <gaaaa.suuuu@gmail.com>

---
+++ js/common.js
@@ -0,0 +1,7 @@
+//load
+$(function(){
+	$("#Loadnav").load("/”Ô‘g–¼DDD/contents_i/nav.html");
+	$("#Loadaside").load("/”Ô‘g–¼DDD/contents_i/aside.html");
+	$("#Loadfooter").load("/”Ô‘g–¼DDD/contents_i/footer.html");
+});
+
